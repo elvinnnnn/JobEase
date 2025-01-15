5819a196bbe9905845ecde436b7d8682186f13c1
Nick Doshi <s3980727@student.rmit.edu.au>
2025-01-15 11:40:33 +1100

    jobController ~ basic method signatures

--- src/controller/JobController.java
+++ src/controller/JobController.java
@@ -1,5 +1,47 @@
 package src.controller;
+import java.util.ArrayList;
+import java.util.List;
+import src.model.JobOpportunity;
+import src.model.dao.JobOpportunityDAO;
+import src.controller.SearchCondition;
 
 public class JobController {
+    // CREATE DATABASE OPERATIONS
+    private JobOpportunityDAO jobDAO;
+
+    // Constructor
+    public void JobController() {
+        
+    }
+    /*
+     * Add a job opportunity to database
+     */
+    public boolean addJobOpportunity(JobOpportunity j) {
+        return jobDAO.addJob(j);
+    }
     
+    /*
+     * Add a list of job opportunities to the database
+     */
+    public boolean addJobOpportunities(List<JobOpportunity> jobs) {
+        return jobDAO.addAllJobs(jobs);
+    }
+
+    // READ DATABASE OPERATIONS
+
+    /* 
+     * Returns all job opportunities in database
+     */
+    public List<JobOpportunity> getAllJobs() {
+
+        return jobDAO.getAllJobs();
+    }
+
+    /*
+     * Returns all job opportunities aligned with search condition
+     */
+
+    public List<JobOpportunity> getAllJobs(SearchCondition con) {
+        return jobDAO.getAllJobs(con);
+    }
 }
